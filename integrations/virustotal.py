45df3aff89f6690e234be1ff69d28507eebe30db
🚀 Dennis Chomba Muchiri | Cybersecurity Professional <dennis.cyberx@gmail.com>
2025-06-26 00:17:04 +0300

    Update virustotal.py

--- integrations/virustotal.py
+++ integrations/virustotal.py
@@ -1,1 +1,10 @@
+import requests
+import os
 
+API_KEY = os.getenv("VT_API_KEY")
+BASE_URL = "https://www.virustotal.com/api/v3/urls"
+
+def scan_url(url):
+    headers = {"x-apikey": API_KEY}
+    response = requests.post(BASE_URL, headers=headers, data={"url": url})
+    return response.json()
